b6a764f9a61366cd60a13b3e0b7d400bb9361bb3
siriglueck <siriluck.rattanapong@gmail.com>
2025-12-01 13:38:54 +0000

    Librarymanger Viewnamen umbenannt

--- librarymanager/app/Http/Controllers/BookController.php
+++ librarymanager/app/Http/Controllers/BookController.php
@@ -6,13 +6,13 @@ class BookController extends Controller
 {
     public function index() 
     {
-        $headline = 'Willkommen im Librarymanager';
+        $headline = 'Willkommen im „LibraryManager – Startseite"';
         $today = now()->format('d.m.Y');
         
         /**
          * Ruft die View home.blade.php auf und übergibt Variableninhalte
          */
-        return view('home', [
+        return view('books', [
             'headline' => $headline,
             'today'    => $today
         ]);
